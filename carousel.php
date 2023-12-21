a4cd6e704cde6abfeebdf5c4275cd866324c8327
Chelsea Johnson <60554690+chelsea-johnson927@users.noreply.github.com>
2023-12-20 21:31:32 -0500

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -36,8 +36,8 @@ if ($loop-> have_posts() ) {
 				$postID = get_the_ID();   
 				$key_1_values = get_post_meta($postID, 'large_fancy_header', true ); 
 				$key_2_values = get_post_meta($postID, 'small_fancy_header', true );  
-				$key_3_values = get_post_meta($postID, 'banner_button', true );
-				$key_4_values = get_post_meta($postID, 'banner_button_text', true );
+				$key_3_values = get_post_meta($postID, 'banner_button', false);   
+                		$innerArray = $key_3_values[0];
 				$featured_img_url = get_the_post_thumbnail_url($postID,'full'); 
 
 
@@ -47,12 +47,13 @@ if ($loop-> have_posts() ) {
 				  						<div class="d-block w-100 div-height" style="background: url('. $featured_img_url . ')"></div> 
 										  <div class="carousel-caption d-none d-md-block">
 										  <h2>' . $key_1_values . '</h2>
-										  <p>' . $key_2_values . '</p>  
-										  <button><a href="' . $key_3_values . '">' . $key_4_values . '</a></button>
-										</div>
-									</div>'; 
+										  <p>' . $key_2_values . '</p> 
+                                          <button><a href="'. esc_url($innerArray['url']) . '">' . esc_html($innerArray['title']) . '</a></button>
+										</div></div>';  
 
-									array_push($allValues,$slideOneHTML);
+                                        
+									array_push($allValues,$slideOneHTML); 
+                                    
 
 				}else{ 
 
@@ -60,11 +61,13 @@ if ($loop-> have_posts() ) {
 										<div class="d-block w-100 div-height" style="background: url('. $featured_img_url . ')"></div>  
 										<div class="carousel-caption d-none d-md-block">
 										<h2>' . $key_1_values . '</h2>
-										<p>' . $key_2_values . '</p>  
-										<button><a href="' . $key_3_values . '">' . $key_4_values . '</a></button>
-									  </div></div>'; 
+										<p>' . $key_2_values . '</p> 
+                                        <button><a href="'. esc_url($innerArray['url']) . '">' . esc_html($innerArray['title']) . '</a></button>
+                                        
+				                        </div></div>'; 
 
-								array_push($allValues,$slideTwoHTML);
+								        array_push($allValues,$slideTwoHTML); 
+                                        
 
 				} 
 
@@ -75,17 +78,10 @@ if ($loop-> have_posts() ) {
 				
 				$string = implode(', ', $allValues);
 
-				$bttnHTML = '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-								<span class="visually-hidden">Previous</span>
-			 				 </button>
-			 				 <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-								<span class="carousel-control-next-icon" aria-hidden="true"></span>
-								<span class="visually-hidden">Next</span>
-			  				</button>';
+				
 	} // end while
 
 
-	return '<div id="carouselExample" class="carousel slide"><div class="carousel-inner">'. $string . $bttnHTML . '</div></div>'; 
+	return '<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel"><div class="carousel-inner"> '. $string . '</div></div>'; 
 
 }add_shortcode('carousel', 'custom_shortcode_function');
